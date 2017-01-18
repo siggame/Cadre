a835cd63319de4ecd8289d403ed89209dc26abdf
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-01-18 13:57:46 -0600

    Added script to ease initializing subrepos
    Also brought subrepos up to date

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script will initialize all the Cadre subrepos for you
+# It simply initializes each subrepository, then checkouts the
+# master branch for each subrepo
+
+git submodule update --init --recursive
+exec ./fordirs.sh Cerveau/ Creer/ Joueur.* run git checkout master
+exec ./fordirs.sh Cerveau/ Creer/ Joueur.* run git pull
