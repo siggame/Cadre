543df835b8fc56333a0b8684d57dfb2097c3b00b
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-02-04 20:20:35 -0600

    Update helper scripts

---
+++ add-mmai-dev-repos.sh
@@ -0,0 +1,12 @@
+s script will add the "public" versions of the Cadre repos
+# As `public` remotes for all applicable repos
+
+for repo in Cerveau Viseur Joueur.cpp Joueur.cs Joueur.java Joueur.js Joueur.lua Joueur.py
+do
+    cd $repo
+    echo " -> adding MMAI-dev branch for $repo"
+    git remote add mmai-dev git@github.com:siggame/$repo-MegaMinerAI-Dev.git
+    git fetch --all
+    cd ..
+done
+
