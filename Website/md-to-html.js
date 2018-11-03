efc5bedf7e7f3517d7accc75892ee758a35518b0
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-11-02 20:00:39 -0500

    Website: Update to use flat-html-helpers npm module

--- Website/md-to-html.js
+++ Website/md-to-html.js
@@ -2,6 +2,7 @@ const fse = require("fs-extra");
 const path = require("path");
 const { promisify } = require("util");
 
+const { cleanIds, sectionize } = require("flat-html-helpers");
 const htmlMinifier = require("html-minifier");
 const marked = require("marked");
 const sass = require("node-sass");
@@ -19,48 +20,7 @@ const OUTPUT_DIR = process.argv[3] || "./";
     // Interestingly these are the only 2 spots TS can't. Otherwise it's smart enough literally everywhere else.
     // Good job TypeScript team!
 
-    /** @type number[] */
-    const levels = [];
-    /** @type {Object.<string, number>} */
-    const ids = {};
-
-    const mdToHtml = marked(md.toString());
-    const html = mdToHtml
-    // First, wrap all sections, and their contents, into sections and divs for easier styling
-    .replace(/(<hr>\n)?<h([1-6]).*?>/g, (s) => {
-        const hr = s.startsWith("<hr>") ? "<hr>" : "";
-        const header = s.replace("<hr>", ""); // remove the hr, so we have only the header
-        const level = Number(header[header.indexOf("<h") + 2]);
-        const last = levels[levels.length - 1];
-        const section = `${hr}<section class="for-h${level}">${header}`;
-
-        if (!last || level > last) {
-            levels.push(level);
-            return section ;
-        }
-        else { // level < last, so we are going out of the current level
-            const diff = (last - level);
-            levels.length = Math.max(0, levels.length - diff);
-
-            const end = "</div></section>".repeat(diff + 1);
-            return end + section;
-        }
-    })
-    // Add the div for the contents as mentioned above
-    .replace(/<\/h([1-6])>/g, (s) => `${s}<div class="h${s[3]}-content">`)
-    // Next, make sure there are no duplicate ids, and if there are add a number on the end of subsequent ids
-    .replace(/id=\"([^"]*)\"/g, (s) => {
-        const id = s.slice(4, s.length - 1);
-        const n = (ids[id] || 0) + 1;
-        ids[id] = n;
-
-        return "id=" + (n > 1 ? `${id}-${n}` : id);
-    })
-    // If a <h3> has an emphasis <em> sub tag, place a <br/> before it.
-    .replace(/<h3(.+?)<em(.+?)\/h3>/g, (s) => s.replace(/<em/g, (sub) => `<br />${sub}`))
-    // Finally close all elements we added in above, to "finish" the html
-    + "</div></section>".repeat(levels.length) + "\n";
-
+    const html = sectionize(cleanIds(marked(md.toString()))); // |> would be ideal here
     const { css } = await sassRender({ file: path.join(__dirname, "style.scss") });
 
     // We don't care about the extra indentation space in this string literal,
