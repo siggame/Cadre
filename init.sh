a43faa90b2709f5ae6d091d56ff2fcbdd21e0df8
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-12-22 11:09:04 -0600

    Update init script to consider all submodules via .gitmodules

--- init.sh
+++ init.sh
@@ -3,6 +3,16 @@
 # It simply initializes each subrepository, then checkouts the
 # master branch for each subrepo
 
+echo ">> Initializing all submodules with git <<"
 git submodule update --init --recursive
-exec ./fordirs.sh Cerveau/ Creer/ Viseur/ Joueur.* run git checkout master
-exec ./fordirs.sh Cerveau/ Creer/ Viseur/ Joueur.* run git pull
+
+submodules=($(git config --file .gitmodules --get-regexp path | awk '{ print $2 }'))
+for submodule in "${submodules[@]}"; do
+    echo "--> ${submodule} <---"
+    cd ${submodule}
+    git checkout master
+    git pull
+    cd ..
+done
+
+echo ">> Cadre repos initialized, exiting <<"
